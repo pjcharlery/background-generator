db14212271db13fb85521125473c1566d56a55e3
jcharlery <jcharlery710@gmail.com>
2020-12-19 19:00:20 -0400

    adding starting project

---
+++ script.js
@@ -0,0 +1,12 @@
+const basket = ['apples', 'oranges', 'grapes'];
+
+for(let i = 0 i < basket.length; i++) {
+    console.log(basket[i]);
+}
+
+
+basket.forEach(item =>{
+    console.log(item);
+    
+    
+})
